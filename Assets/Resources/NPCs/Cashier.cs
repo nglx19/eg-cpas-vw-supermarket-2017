f72be838e3e53c9edc7538ce95437a3c704900fe
needcomms <needcomms@gmail.com>
2017-08-26 14:28:36 +0800

    Added Cashier NPC
    Added Cashier NPC animation

---
+++ Assets/Resources/NPCs/Cashier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cashier : MonoBehaviour {
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+        if(info.IsName("end_of_scan"))
+        {
+            bool finished = (Random.Range(0f, 1f) < 0.25f? true: false);
+            anim.SetBool("finished", finished);
+        }
+    }
+}
